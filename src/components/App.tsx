9fb67f02f700340fe7ecafb79020590907318b66
Mohamed Anis KOUA <102376766+zaixrx@users.noreply.github.com>
2025-06-12 02:28:46 +0100

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -53,7 +53,7 @@ function App() {
     let average = result;
     average -= module.totalGrade * (module.coeffecient / 27);
     module.totalGrade = clamp(gradeExam * (2 / 3) + gradeTD * (1 / 3), 0, 20);
-    average += module.totalGrade * (module.coeffecient / 26);
+    average += module.totalGrade * (module.coeffecient / 27);
 
     setAverage(average);
     setModules(_modules);
